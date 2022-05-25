ddd930fe809c429cf8545b4529ce92f59eb8c35f
Muhammad-Saeed-0 <muhammad01111417159@gmail.com>
2022-05-25 22:46:37 +0200

    ..

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# Note that this code uses %{…}. This is an alternative syntax for doublequoted string literals,
+# convenient for use with long strings. Note also that because it uses the Rails create!() method,
+# it’ll raise an exception if records can’t be inserted because of validation errors.)
 #
-# Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.delete_all
+# . . .
+Product.create!(title: 'Build Chatbot Interactions', description: %{Responsive, Intuitive Interfaces with Ruby
+The next step in the evolution of user interfaces is here.
+Chatbots let your users interact with your service in their
+own natural language. Use free and open source tools along
+with Ruby to build creative, useful, and unexpected interactions
+for users. Take advantage of the Lita framework’s step-by-step
+implementation strategy to simplify bot development and testing.
+From novices to experts, chatbots are an area in which everyone
+can participate. Exercise your creativity by creating chatbot
+skills for communicating, information, and fun.},
+ image_url: 'dpchat.jpg',
+ price: 20.00)
+# . . .
+#
